efcdb2e3050bbcb6cbccd62232b8d9d36027e16b
EC2 Default User <ec2-user@ip-172-31-16-243.us-east-2.compute.internal>
2021-02-07 17:12:37 +0000

    hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,74 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  nums = []
+  if arr.length == 1
+    nums = arr
+  end
+  if arr.length >= 2
+    nums = arr.max(2)
+  end
+  nums.inject(0, :+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length <= 1
+    return false
+  else
+    arr.each do |i|
+      if arr.include?(n - i) && i * 2 != n
+        return true
+      end
+    end
+    return false 
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false 
+  elsif s =~ /^[^a-zA-Z].*/
+    return false
+  elsif s =~ /^[aeiouAEIOU].*/
+    return false 
+  else 
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  elsif s == "0" || s =~ /^[01]*00$/
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize isbn, price
+    if isbn.empty? || price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
